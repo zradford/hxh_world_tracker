4b722f9e88ed32206dc5c1be13ced1a8f9efd03c
Zac <zac@MacBook-Pro.lan>
2023-08-12 15:09:13 -0500

    changed to postgres, using .env

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_11_181459) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "abilities", force: :cascade do |t|
     t.string "name", null: false
     t.string "uses"
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_181459) do
     t.text "effect", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "character_id"
+    t.bigint "character_id"
     t.boolean "is_passive"
     t.index ["character_id"], name: "index_abilities_on_character_id"
   end
@@ -41,13 +44,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_181459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "created_by_user_id"
-    t.integer "in_use_by_user_id"
+    t.bigint "in_use_by_user_id"
     t.integer "ability_count", default: 4
     t.index ["in_use_by_user_id"], name: "index_characters_on_in_use_by_user_id"
   end
 
   create_table "notes", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "title"
     t.text "body"
     t.boolean "private"
